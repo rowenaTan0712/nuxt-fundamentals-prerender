ba026f018232fda1114d6e7025002dd9e6e2a83b
Ma. Rowena Tan <rowena.tan0712@gmail.com>
2020-02-05 17:51:18 +0800

    initial commit

---
+++ components/Navigation.vue
@@ -0,0 +1,3 @@
+<template>
+<p> This is a Sample Header... </p>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        nuxt-fundamentals-prerender
+      </h1>
+      <h2 class="subtitle">
+        My spectacular Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+
+        <nuxt-link
+          v-for="post in posts"
+          :to="{name: 'posts-id', params: {id: post.id}}"
+          :key="post.id"
+          class="button--grey"
+        >
+          {{post.title}}
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+
+  data () {
+     return {
+       posts: [
+         {
+           id: 'balut',
+           title: 'What is Balut?',
+           content: 'If someone placed balut on your plate, you might think they were serving you a hardboiled egg. That is, until you cracked it open and a fully intact duck embryo spilled out. Balut, considered a delicacy in many Asian countries, is produced when fertilized duck eggs are placed in warm sunlight. After about eight days, the eggs are held up to the light and checked to ensure that the budding embryo is ready. Then, the eggs are cooked and served with a dash of salt and a few squirts of lemon juice.'
+         },
+         {
+           id: 'whereIsIt',
+           title: 'Where is the sign in button??',
+           content: 'I\'ve stalked the forum for a while, reading everything closely, but today I decided that it was time to register as a user. So I did, this morning! But now, when I came back to the forum, I cannot seem to find the damn sign in button? WHERE IS IT???!??'
+         },
+         {
+           id: 'how',
+           title: 'How can I fix this problem?',
+           content: 'Please help me. I dont know what is the best way to fix my problem. I cant reproduce and I cant tell more info. Can someone help me asap??? I am in a hurry!!!'
+         }
+       ]
+     }
+   },
+
+  head () {
+    return {
+        title : "Wena's Home Page",
+        meta : [
+          {name : "og:title", content : this.posts[0].title},
+          {name : "og:description", content : this.posts[0].content}
+        ]
+    }
+  }
+
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
